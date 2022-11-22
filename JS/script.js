0d4d3b36717b339ab2695ffb84cd0cde79154e65
Glenda Diaz <glendadiazah@outlook.com>
2022-11-22 14:33:02 -0600

    html, css and media for main program section

--- JS/script.js
+++ JS/script.js
@@ -1,17 +1,20 @@
 const openMenu = document.querySelector('#menu-button');
 const closeButton = document.querySelector('.close-button');
 const menu = document.querySelector('.menu-items');
+const bak = document.querySelector('.button-container');
 
 function OpenM(){
     menu.style.display = 'flex';
     closeButton.style.display = 'block';
     openMenu.style.display = 'none';
+    bak.style.display = 'block';
 }
 
 function CloseM(){
     menu.style.display = 'none';
     closeButton.style.display = 'none';
     openMenu.style.display = 'block';
+    bak.style.display = 'none';
 }
 
 openMenu.addEventListener('click',OpenM);
