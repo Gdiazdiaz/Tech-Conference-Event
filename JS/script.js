46d95ef6ef9ba29409db3c979ba362e4b41e239e
Glenda Diaz <glendadiazah@outlook.com>
2022-11-22 18:16:48 -0600

    add speakers section in JS, footer in html and css

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,7 @@ const openMenu = document.querySelector('#menu-button');
 const closeButton = document.querySelector('.close-button');
 const menu = document.querySelector('.menu-items');
 const bak = document.querySelector('.button-container');
+const cards = document.querySelector('.spk-card-container');
 
 function OpenM(){
     menu.style.display = 'flex';
@@ -19,3 +20,60 @@ function CloseM(){
 
 openMenu.addEventListener('click',OpenM);
 closeButton.addEventListener('click',CloseM);
+
+const cardlist = [
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+    {
+    imgUrl: 'CSS/media/card-pictures/spk-1.png',
+    name: 'Glenda Diaz',
+    title: 'Math Professor and Full Stack Developer',
+    description: 'Glenda studied her Bachelor in math and is also an graduate from Microverse Full Stack Program',
+    },
+];
+
+let cardgenerator ='';
+
+cardlist.forEach((card) => {
+    cardgenerator += `
+    <!-- recent work card ${card.id + 1} -->
+    <div class="spk-card">
+                <img class="spk-card-img" src="${card.imgUrl}">
+                <div class="spk-card-content">
+                    <h4 class="spk-card-name">${card.name}</h4>
+                    <h5 class="spk-card-title">${card.title}</h5>
+                    <p class="spk-card-text">${card.description}</p>
+                </div>
+            </div>
+    `;
+});
+
+cards.innerHTML += cardgenerator;
